76758508fa7e4f9f3bd4dd82ebad861b26111b87
Adam Smith <adam.smith@electralink.co.uk>
2020-10-08 12:37:05 +0100

    Step 6

---
+++ app.py
@@ -0,0 +1,16 @@
+# https://medium.com/bettercode/how-to-build-a-modern-ci-cd-pipeline-5faa01891a5b
+import os
+from flask import Flask
+from buzz import generator
+
+app = Flask(__name__)
+
+@app.route("/")
+def generate_buzz():
+    page = '<html><body><h1>'
+    page += generator.generate_buzz()
+    page += '</h1></body></html>'
+    return page
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
