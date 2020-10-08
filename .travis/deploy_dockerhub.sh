8a937d2535c24917d5474b9584b2a809085c9724
Adam Smith <adam.smith@electralink.co.uk>
2020-10-08 13:04:29 +0100

    Step 8

---
+++ .travis/deploy_dockerhub.sh
@@ -0,0 +1,10 @@
+# https://medium.com/bettercode/how-to-build-a-modern-ci-cd-pipeline-5faa01891a5b
+#!/bin/sh
+docker login -u $DOCKER_USER -p $DOCKER_PASS
+if [ "$TRAVIS_BRANCH" = "master" ]; then
+    TAG="latest"
+else
+    TAG="$TRAVIS_BRANCH"
+fi
+docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
+docker push $TRAVIS_REPO_SLUG:$TAG
